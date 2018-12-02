e2eddd7dc2540773f6f8123c4f0b75c1231f168f
Tom Spettigue <apickle@gmail.com>
2018-12-02 15:25:13 -0700

    Edited some variables in 'Get-LastDomainAndEmail'

--- Windows/Get-LastDomainAndEmail/Get-LastDomainAndEmail.ps1
+++ Windows/Get-LastDomainAndEmail/Get-LastDomainAndEmail.ps1
@@ -1,5 +1,4 @@
-$users = Get-Content 'F:\CRE_Move\CRE-user-list.txt'
-# $users = Read-Host 'Enter the full path to the text file containing a list of usernames, with one username per line'
+$users = Read-Host 'Enter the full path to the text file containing a list of usernames, with one username per line'
 $csv = Read-Host 'Enter the full path to where you want the .csv file to be saved'
 
 echo "Name,Username,Last Logon,ProxyAddresses" > $csv
