45356ae49a22a8a270cb8164409e6949b3d2ff87
Tom Spettigue <apickle@gmail.com>
2018-12-02 21:10:50 -0700

    THAT wasn't supposed to go up there...

---
+++ Windows/DriveLetterVoteCounter/DriveLetterVoteCounter.ps1
@@ -0,0 +1,11 @@
+# Name:		Drive Letter Vote Counter
+# Purpose: 	Script for determining which drive letters should correspond to what
+#			shared network resource. Best for "organizations" that don't know
+#			what the word means, and who's drive mappings are all over the
+#			place, and for whom their systems administrator holds justified
+#			contempt for having to tie it all back up. This is for you, sysadmin.
+# Author:	Tom Spettigue
+# Version:	0.0.1
+# Date:		2 December 2018
+
+foreach
